7df6138d88c82324407a33fa49ad560c6efbb4d4
StekAlaZarii <101180506+StekAlaZarii@users.noreply.github.com>
2022-05-17 09:40:09 +0000

    Dodawanie studentów z konsoli

--- Main.java
+++ Main.java
@@ -1,18 +1,59 @@
 import java.io.IOException;
+import java.util.Scanner;
 
 class Main {
   public static void main(String[] args) {
+    
+    while(true){
+      int opcja=0;
+      opcja=menu();
+
+      switch(opcja)
+      {
+        case 1:{
+          System.out.println();
+          OP1();
+          break;
+        }
+        case 0:{
+          return;
+        }
+      }      
+    }
+
+  }
+
+  public static int menu(){
+    Scanner skaner = new Scanner(System.in);
+    int numer=0;
+
+    System.out.println();
+    System.out.println();
+    System.out.println("Menu: ");
+    System.out.println("1. Dodaj studenta");
+    System.out.println("0. Wyjście z programu");
+    System.out.print("Wybierz opcję: ");
+    
+    numer = skaner.nextInt();
+    return numer;
+  }
+
+  public static void OP1(){
+    Scanner skaner = new Scanner(System.in);
     try {
       Service1 s = new Service1();
-      s.addStudent(new Student("Krzysztof", 20));
-      s.addStudent(new Student("Janusz", 40));
+      int wiek;
+      String imie;
 
-      var students = s.getStudents();
-      for(Student current : students) {
-        System.out.println(current.ToString());
-      }
+      System.out.print("Podaj imie: ");
+      imie=skaner.nextLine();
+      System.out.print("Podaj wiek: ");
+      wiek=skaner.nextInt();
+          
+      s.addStudent(new Student(imie, wiek));
     } catch (IOException e) {
 
     }
   }
+  
 }
